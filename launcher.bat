6641ae98607f43519873dbf34c9230a64bf408f9
aizwellenstan <aizwellenstan@gmail.com>
2023-07-06 17:21:57 +0800

    add mod

--- launcher.bat
+++ launcher.bat
@@ -8,10 +8,13 @@ set OCIO=%~dp0tool\settings\ocio\v5\config.ocio
 
 set MAYA_SHELF_PATH=%~dp0shelves
 set MAYA_APP_DIR=%userprofile%\Documents\projects\%PROJNAME%\maya
-if not exist %MAYA_APP_DIR% (
-    mkdir %MAYA_APP_DIR%
-)
+if not exist %MAYA_APP_DIR% mkdir %MAYA_APP_DIR%
 
 robocopy %~dp0scripts %MAYA_APP_DIR%\scripts\ /it
+
+set MODULE_FOLDER=%MAYA_APP_DIR%\modules
+if not exist %MODULE_FOLDER% mkdir %MODULE_FOLDER%
+
+robocopy %~dp0mod %MODULE_FOLDER%\ /it /E /IS /IT
 
 call "C:\Program Files\Autodesk\Maya%MAYA_VERSION%\bin\maya.exe"
---
+++ mod/mtoa.mod
@@ -0,0 +1,8 @@
++ mtoa any C:\Program Files\Autodesk\Arnold\maya2023
+PATH +:= bin
+PATH +:= procedurals
+ARNOLD_PLUGIN_PATH +:= shaders
+MTOA_EXTENSIONS_PATH +:= extensions
+MAYA_PLUG_IN_PATH +:=  plug-ins
+MAYA_SCRIPT_PATH +:= scripts/mtoa
+MAYA_CUSTOM_TEMPLATE_PATH +:= scripts/mtoa/ui/templates
